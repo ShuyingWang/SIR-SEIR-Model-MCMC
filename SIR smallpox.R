c9b9c54aacf5a2f0b947f659d796f9a0c9cacfd8
ShuyingWang <40690641+ShuyingWang@users.noreply.github.com>
2023-04-25 15:18:43 -0700

    Add files via upload

--- SIR smallpox.R
+++ SIR smallpox.R
@@ -1,13 +1,13 @@
 ### Description of the code file ###
 # The first part defines the functions.
 # The second part includes the illustration with the smallpox dataset (Section 4.3 in the paper).
-# Comment/uncomment the new/current MCMC algorithms to run comparisons.
 
+library(mcmcse)
+library(ReIns)
 
 ### 1. Functions ###
 
-# Integrals (for calculating likelihoods)
-
+# Likelihood calculation
 SI_int <- function(t.x, t.y, n.x, n.y, t0, T.end){
   t = sort(c(t.x, t.y, t0, T.end))
   n = n.x + n.y + 1
@@ -26,7 +26,6 @@ SI_int <- function(t.x, t.y, n.x, n.y, t0, T.end){
   }
   return(int)
 }
-
 I_int <- function(t.x, t.y, n.x, n.y, t0, T.end){
   t = sort(c(t.x, t.y, t0, T.end))
   n = n.x + n.y + 1
@@ -43,10 +42,6 @@ I_int <- function(t.x, t.y, n.x, n.y, t0, T.end){
   }
   return(int)
 }
-
-
-# Products (for calculating likelihoods)
-
 Log.SI_prod <- function(t.x, t.y){
   times = sort(c(t.x, t.y))
   S = S0
@@ -64,7 +59,6 @@ Log.SI_prod <- function(t.x, t.y){
   }
   return(lp)
 }
-
 Log.I_prod <- function(t.x, t.y){
   times = sort(c(t.x, t.y))
   I = I0
@@ -80,7 +74,6 @@ Log.I_prod <- function(t.x, t.y){
   }
   return(lp)
 }
-
 Indicator <- function(t.x, t.y){
   times = sort(c(t.x, t.y))
   S = S0
@@ -106,7 +99,7 @@ Indicator <- function(t.x, t.y){
 
 
 # Conversions
-X.to.u <- function(t.x, t.y, t0, T.end, beta){
+X_to_u <- function(t.x, t.y, t0, T.end, beta){
   t.x = c(t.x, T.end)
   t = sort(c(t.x, t.y, t0))
   S = S0
@@ -126,10 +119,16 @@ X.to.u <- function(t.x, t.y, t0, T.end, beta){
     }
   }
   U = U * beta / N
+  n = length(U)
+  U[n] = U[n] + rexp(1, 1)
   return(U)
 }
-
-u.to.X <- function(u, t.y, t0, T.end, beta){
+u_to_u.new <- function(u, r){
+  n = length(u)
+  u.new = ifelse(rbinom(n,1,r), rexp(n,1), u)
+  return(u.new)
+}
+u_to_X <- function(u, t.y, t0, T.end, beta){
   t.y = c(t.y, T.end)
   n = length(u)
   S = S0
@@ -161,14 +160,215 @@ u.to.X <- function(u, t.y, t0, T.end, beta){
 }
 
 
+# New MCMC algorithm 
+New_proposal.x <- function(t.y, n.y, beta, t0, T.end){
+  ty = c(t.y, T.end)
+  S = S0
+  I = I0
+  t = t0
+  tx.new = c()
+  i = 1
+  while (t < T.end & S > 0 & I > 0){
+    t_new = t + rexp(1, beta/N * S * I)
+    if (I==1 & i<n.y & t_new>ty[i]){
+      S = S0
+      I = I0
+      t = t0
+      tx.new = c()
+      i = 1
+    }else if (t_new < ty[i]){
+      t = t_new
+      tx.new = c(tx.new, t)
+      I = I + 1
+      S = S - 1
+    }else{
+      t = ty[i]
+      I = I - 1
+      i = i + 1
+    }
+  }
+  return(tx.new)
+}
+New_MH_adjust.x <- function(r, t.x, n.x, t.y, n.y, beta, gamma, t0, T.end){
+  
+  # update initial infection time
+  t0 = t.x[1]-rexp(1, gamma + beta*S0/N + 0.1)
+  
+  # update beta and gamma
+  beta = rgamma(1, n.x+10, SI_int(t.x, t.y, n.x, n.y, t0, T.end)/N+100)
+  gamma = rgamma(1, n.y+10, I_int(t.x, t.y, n.x, n.y, t0, T.end)+100)
+  
+  # update X
+  u = X_to_u(t.x, t.y, t0, T.end, beta)
+  u.new = u_to_u.new(u, r)
+  tx.new = u_to_X(u.new, t.y, t0, T.end, beta)
+  nx.new = length(tx.new)
+  acc = 0
+  if (Indicator(tx.new, t.y)){
+    log_p = Log.I_prod(t.x, t.y) - gamma * I_int(t.x, t.y, n.x, n.y, t0, T.end)
+    log_p.new = Log.I_prod(tx.new, t.y) - gamma * I_int(tx.new, t.y, nx.new, n.y, t0, T.end)
+    log_a = log_p.new-log_p
+    acc = 0
+    if (log(runif(1)) < log_a){
+      t.x = tx.new
+      n.x = nx.new
+      acc = 1
+    }
+  }
+  return(list("t0"=t0, "beta"=beta, "gamma"=gamma, "tx"=t.x, "nx"=n.x, "acc"=acc))
+}
+
+# Current MCMC algorithm (Random Walk Metropolis)
+Current_MH.x <- function(k, t.x, n.x, t.y, n.y, beta, gamma, t0, T.end){
+  
+  # update initial infection time
+  t0 = t.x[1]-rexp(1, gamma + beta*S0/N + 0.1)
+  
+  # update beta and gamma
+  beta = rgamma(1, n.x+10, SI_int(t.x, t.y, n.x, n.y, t0, T.end)/N+100)
+  gamma = rgamma(1, n.y+10, I_int(t.x, t.y, n.x, n.y, t0, T.end)+100)
+  
+  # update X
+  tx.new = t.x
+  log_a = 0
+  for (i in 1:k){
+    u = runif(1, 0, 1)
+    if (u < 1/3){
+      # Add one time point
+      tx.new = sort(c(runif(1, t0, T.end), tx.new))
+      log_a = log_a + log(T.end-t0) - log(length(tx.new))
+    }else{
+      if (u > 2/3){
+        # Move one time point
+        tx.new = tx.new[-sample(1:length(tx.new), 1)]
+        tx.new = sort(c(runif(1, t0, T.end), tx.new))
+      }else{
+        # Remove one time point
+        tx.new = tx.new[-sample(1:length(tx.new), 1)]
+        log_a = log_a + log(length(tx.new)+1) - log(T.end-t0)
+      }
+    }
+  }
+  nx.new = length(tx.new)
+  acc = 0
+  if (Indicator(tx.new, t.y)){
+    log_p = n.x * log(beta/N) + Log.SI_prod(t.x, t.y) - beta/N * SI_int(t.x, t.y, n.x, n.y, t0, T.end) +
+      Log.I_prod(t.x, t.y) - gamma * I_int(t.x, t.y, n.x, n.y, t0, T.end)
+    log_p.new = nx.new * log(beta/N) + Log.SI_prod(tx.new, t.y) - beta/N * SI_int(tx.new, t.y, nx.new, n.y, t0, T.end) +
+      Log.I_prod(tx.new, t.y) - gamma * I_int(tx.new, t.y, nx.new, n.y, t0, T.end)
+    log_a = log_a + log_p.new - log_p
+    if (log(runif(1)) < log_a){
+      t.x = tx.new
+      n.x = nx.new
+      acc = 1
+    }
+  }
+  return(list("t0"=t0, "beta"=beta, "gamma"=gamma, "tx"=t.x, "nx"=n.x, "acc"=acc))
+}
+
+# Simulation-based Bayesian inference for epidemic models
+Simulation_MH.x <- function(t.x, n.x, t.y, n.y, beta, gamma, t0, T.end){
+  
+  # update initial infection time
+  t0 = t.x[1]-rexp(1, gamma + beta*S0/N + 0.1)
+  
+  # update beta and gamma
+  beta = rgamma(1, n.x+10, SI_int(t.x, t.y, n.x, n.y, t0, T.end)/N+100)
+  gamma = rgamma(1, n.y+10, I_int(t.x, t.y, n.x, n.y, t0, T.end)+100)
+  
+  # update X
+  S = S0
+  I = I0
+  t = t0
+  tx.new = c()
+  lp.new = 0
+  for (i in 0:(T.end-1)){
+    ny = sum(t.y==i)
+    nx = ny+1 - I
+    if (nx > 0){
+      for (j in 1:nx){
+        lp.new = lp.new + log(1 - exp(-beta*S*I/N * (i-t)))
+        t = t + rtexp(1, beta/N * S * I, i-t)
+        tx.new = c(tx.new, t)
+        I = I + 1
+        S = S - 1
+      }
+    }
+    if (S > 0){
+      t_new = t + rexp(1, beta/N * S * I)
+      while (t_new < i){
+        t = t_new
+        tx.new = c(tx.new, t)
+        I = I + 1
+        S = S - 1
+        if (S == 0){
+          break
+        }
+        t_new = t + rexp(1, beta/N * S * I) 
+      }
+    }
+    t = i
+    I = I - ny
+  }
+  if (S > 0){
+    t_new = t + rexp(1, beta/N * S * I)
+    while (t_new < T.end){
+      t = t_new
+      tx.new = c(tx.new, t)
+      I = I + 1
+      S = S - 1
+      if (S == 0){
+        break
+      }
+      t_new = t + rexp(1, beta/N * S * I)
+    }
+  }
+  nx.new = length(tx.new)
+  log_p.new = lp.new + Log.I_prod(tx.new, t.y) - gamma * I_int(tx.new, t.y, nx.new, n.y, t0, T.end)
+  
+  S = S0
+  I = I0
+  t = t0
+  k = 1
+  tx = c(t.x, T.end)
+  lp = 0
+  for (i in 0:(T.end-1)){
+    ny = sum(t.y==i)
+    nx = ny+1 - I
+    if (nx > 0){
+      for (j in 1:nx){
+        lp = lp + log(1 - exp(-beta*S*I/N * (i-t)))
+        t = tx[k]
+        k = k + 1
+        I = I + 1
+        S = S - 1
+      }
+    }
+    while (tx[k] < i){
+      t = tx[k]
+      k = k + 1
+      I = I + 1
+      S = S - 1
+    }
+    t = i
+    I = I - ny
+  }
+  log_p = lp + Log.I_prod(t.x, t.y) - gamma * I_int(t.x, t.y, n.x, n.y, t0, T.end)
+  acc = 0
+  if (log(runif(1)) < (log_p.new-log_p)){
+    t.x = tx.new
+    n.x = nx.new
+    acc = 1
+  }
+  return(list("t0"=t0, "beta"=beta, "gamma"=gamma, "tx"=t.x, "nx"=n.x, "acc"=acc))
+}
+
 
 
 
 ### 2. Illustration with smallpox data ###
 
-library(mcmcse)
-
-## Data entry
+# Data entry
 I0 = 1
 N = 120
 S0 = 119
@@ -177,169 +377,183 @@ t.y = cumsum(t.y)
 T.end = 76
 n.y = length(t.y)
 
-## MCMC
-M = 5000
-gamma = 0.09
+
+
+# New MCMC
+set.seed(3)
+B = 10000
+gamma = 0.1
 beta = 0.1
 t.x = t.y - rexp(n.y, 1)
 t0 = t.x[1]-rexp(1, gamma + beta)
 t.x = sort(c(t.x, runif(10, t0, T.end)))
 n.x = length(t.x)
-sample.nx = c()
-sample.t0 = c()
-sample.beta = c()
-sample.gamma = c()
-acc = 0
-
-ptm <- proc.time()
-for (j in 1:M){
-  
-  # update initial infection time
-  t0 = t.x[1]-rexp(1, gamma + beta*S0/N + 0.1)
-  
-  # update beta and gamma
-  beta = rgamma(1, n.x + 10, SI_int(t.x, t.y, n.x, n.y, t0, T.end)/N + 100)
-  gamma = rgamma(1, n.y + 10, I_int(t.x, t.y, n.x, n.y, t0, T.end) + 100)
-
-  
-  ## update latent infection time by new MCMC algorithm
-  # u = X.to.u(t.x, t.y, t0, T.end, beta)
-  # k = ceiling((n.x + 1) * 0.05)
-  # 
-  # valid_proposal = FALSE
-  # while (valid_proposal == FALSE){
-  #   u.new = u
-  #   u.new[sample(1:(n.x+1), k, replace = FALSE)] = rexp(k, 1)
-  #   tx.new = u.to.X(u.new, t.y, t0, T.end, beta)
-  #   nx.new = length(tx.new)
-  #   valid_proposal = Indicator(tx.new, t.y)
-  # }
-  # log_p.new = Log.I_prod(tx.new, t.y) - gamma * I_int(tx.new, t.y, nx.new, n.y, t0, T.end)
-  # log_p.old = Log.I_prod(t.x, t.y) - gamma * I_int(t.x, t.y, n.x, n.y, t0, T.end)
-  # log_p = log_p.new - log_p.old
-  # u = runif(1)
-  # if(log(u) < log_p){
-  #   t.x = tx.new
-  #   n.x = nx.new
-  #   acc = acc + 1
-  # }
-  
-  
-  # update latent infection time by current MCMC algorithm (Random Walk Metropolis)
-  valid_proposal = FALSE
-  while (valid_proposal == FALSE){
-    u = runif(1)
-    if (u < 1/3){
-      # Add one time point
-      nx.new = n.x + 1
-      t.new = runif(1, t0, T.end)
-      tx.new = sort(c(t.new, t.x))
-      move_type = 1
-    }else{
-      if (u > 2/3){
-        # Move one time point
-        nx.new = n.x
-        t.new = runif(1, t0, T.end)
-        tx.new = t.x[-sample(1:n.x, 1)]
-        tx.new = sort(c(t.new, tx.new))
-        move_type = 2
-      }else{
-        # Remove one time point
-        nx.new = n.x - 1
-        tx.new = t.x[-sample(1:n.x, 1)]
-        move_type = 3
-      }
-    }
-    valid_proposal = Indicator(tx.new, t.y)
-  }
-  log_p.new = nx.new * log(beta/N) + Log.SI_prod(tx.new, t.y) - beta/N * SI_int(tx.new, t.y, nx.new, n.y, t0, T.end) +
-    Log.I_prod(tx.new, t.y) - gamma * I_int(tx.new, t.y, nx.new, n.y, t0, T.end)
-  log_p.old = n.x * log(beta/N) + Log.SI_prod(t.x, t.y) - beta/N * SI_int(t.x, t.y, n.x, n.y, t0, T.end) +
-    Log.I_prod(t.x, t.y) - gamma * I_int(t.x, t.y, n.x, n.y, t0, T.end)
-  if (move_type == 1){
-    # Add one time point
-    log_p = log_p.new + log(T.end-t0) - (log_p.old + log(n.x+1))
-  }else{
-    if (move_type == 3){
-      # Remove one time point
-      log_p = log_p.new + log(n.x) - (log_p.old + log(T.end-t0))
-    }else{
-      # Move one time point
-      log_p = log_p.new - log_p.old
-    }
-  }
-  u = runif(1)
-  if(log(u) < log_p){
-    t.x = tx.new
-    n.x = nx.new
-    acc = acc + 1
-  }
-  
-  sample.nx[j] = n.x
-  sample.t0[j] = t0
-  sample.beta[j] = beta
-  sample.gamma[j] = gamma
+sample_nx.new = rep(NA, B)
+sample_t0.new = rep(NA, B)
+sample_beta.new = rep(NA, B)
+sample_gamma.new = rep(NA, B)
+acc.new = 0
+r = 0.05
+for (i in 1:B){
+  Sample = New_MH_adjust.x(r, t.x, n.x, t.y, n.y, beta, gamma, t0, T.end)
+  t.x = Sample$tx
+  n.x = Sample$nx
+  t0 = Sample$t0
+  beta = Sample$beta
+  gamma = Sample$gamma
+  acc.new = acc.new + Sample$acc
+  sample_nx.new[i] = n.x
+  sample_t0.new[i] = t0
+  sample_beta.new[i] = beta
+  sample_gamma.new[i] = gamma
+  print(i)
 }
-proc.time() - ptm
-acc/M
+acc.new/B #0.2784
 
-acc.new = acc/M #0.204
-acc.current = acc/M #0.6398
 
-Samples.new = list(nx=sample.nx, t0=sample.t0, beta=sample.beta, gamma=sample.gamma)
+
+# Random walk MCMC
+set.seed(3)
+B = 10000
+gamma = 0.1
+beta = 0.1
+t.x = t.y - rexp(n.y, 1)
+t0 = t.x[1]-rexp(1, gamma + beta)
+t.x = sort(c(t.x, runif(10, t0, T.end)))
+n.x = length(t.x)
+sample_nx.current = rep(NA, B)
+sample_t0.current = rep(NA, B)
+sample_beta.current = rep(NA, B)
+sample_gamma.current = rep(NA, B)
+acc.current = 0
+k = 5
+for (i in 1:B){
+  Sample = Current_MH.x(k, t.x, n.x, t.y, n.y, beta, gamma, t0, T.end)
+  t.x = Sample$tx
+  n.x = Sample$nx
+  t0 = Sample$t0
+  beta = Sample$beta
+  gamma = Sample$gamma
+  acc.current = acc.current + Sample$acc
+  sample_nx.current[i] = n.x
+  sample_t0.current[i] = t0
+  sample_beta.current[i] = beta
+  sample_gamma.current[i] = gamma
+  print(i)
+}
+acc.current/B # 0.2556
+
+
+
+# Simulation-based MCMC
+set.seed(3)
+B = 10000
+gamma = 0.1
+beta = 0.1
+t.x = t.y - rexp(n.y, 1)
+t0 = t.x[1]-rexp(1, gamma + beta)
+t.x = sort(c(t.x, runif(10, t0, T.end)))
+n.x = length(t.x)
+sample_nx.simulate = rep(NA, B)
+sample_t0.simulate = rep(NA, B)
+sample_beta.simulate = rep(NA, B)
+sample_gamma.simulate = rep(NA, B)
+acc.simulate = 0
+for (i in 1:B){
+  Sample = Simulation_MH.x(t.x, n.x, t.y, n.y, beta, gamma, t0, T.end)
+  t.x = Sample$tx
+  n.x = Sample$nx
+  t0 = Sample$t0
+  beta = Sample$beta
+  gamma = Sample$gamma
+  acc.simulate = acc.simulate + Sample$acc
+  sample_nx.simulate[i] = n.x
+  sample_t0.simulate[i] = t0
+  sample_beta.simulate[i] = beta
+  sample_gamma.simulate[i] = gamma
+  print(i)
+}
+acc.simulate/B # 0.004
+
+
+
+
+Samples.new = list(nx=sample_nx.new, t0=sample_t0.new, beta=sample_beta.new, gamma=sample_gamma.new)
 save(Samples.new, file='new.RData')
-Samples.current = list(nx=sample.nx, t0=sample.t0, beta=sample.beta, gamma=sample.gamma)
+Samples.current = list(nx=sample_nx.current, t0=sample_t0.current, beta=sample_beta.current, gamma=sample_gamma.current)
 save(Samples.current, file='current.RData')
+Samples.simulate = list(nx=sample_nx.simulate, t0=sample_t0.simulate, beta=sample_beta.simulate, gamma=sample_gamma.simulate)
+save(Samples.simulate, file='simulate.RData')
 
 ## Plots
-par(mfrow=c(2, 1))
+par(mfrow=c(3, 1))
 plot(Samples.new$nx, type = 'l',
      main = expression(paste('Trace plot of ', n[x], ' with new MCMC algorithm')),
      ylab = expression(n[x]))
 plot(Samples.current$nx, type = 'l',
      main = expression(paste('Trace plot of ', n[x], ' with current MCMC algorithm')),
      ylab = expression(n[x]))
+plot(Samples.simulate$nx, type = 'l',
+     main = expression(paste('Trace plot of ', n[x], ' with simulation-based MCMC algorithm')),
+     ylab = expression(n[x]))
 
-par(mfrow=c(2, 1))
+par(mfrow=c(3, 1))
 plot(Samples.new$t0, type = 'l', main = expression(paste('Trace plot of ', t[0]^x, ' with new MCMC algorithm')),
      ylab = expression(t[0]^x))
 plot(Samples.current$t0, type = 'l', main = expression(paste('Trace plot of ', t[0]^x, ' with current MCMC algorithm')),
      ylab = expression(t[0]^x))
+plot(Samples.simulate$t0, type = 'l', main = expression(paste('Trace plot of ', t[0]^x, ' with simulation-based MCMC algorithm')),
+     ylab = expression(t[0]^x))
 
-par(mfrow=c(2, 1))
+par(mfrow=c(3, 1))
 plot(Samples.new$beta, type = 'l', main = expression(paste('Trace plot of ', beta, ' with new MCMC algorithm')),
      ylab = expression(beta))
 plot(Samples.current$beta, type = 'l', main = expression(paste('Trace plot of ', beta, ' with current MCMC algorithm')),
      ylab = expression(beta))
+plot(Samples.simulate$beta, type = 'l', main = expression(paste('Trace plot of ', beta, ' with simulation-based MCMC algorithm')),
+     ylab = expression(beta))
 
-par(mfrow=c(2, 1))
+par(mfrow=c(3, 1))
 plot(Samples.new$gamma, type = 'l', main = expression(paste('Trace plot of ', gamma, ' with new MCMC algorithm')),
      ylab = expression(gamma))
 plot(Samples.current$gamma, type = 'l', main = expression(paste('Trace plot of ', gamma, ' with current MCMC algorithm')),
      ylab = expression(gamma))
+plot(Samples.simulate$gamma, type = 'l', main = expression(paste('Trace plot of ', gamma, ' with simulation-based MCMC algorithm')),
+     ylab = expression(gamma))
 
-par(mfrow=c(2, 1))
+par(mfrow=c(3, 1))
 hist(Samples.new$beta, freq = FALSE, main = expression(paste('Histogram of ', beta, ' with new MCMC algorithm')),
      xlab = expression(beta), xlim=c(0.04, 0.2))
 hist(Samples.current$beta, freq = FALSE, main = expression(paste('Histogram of ', beta, ' with current MCMC algorithm')),
      xlab = expression(beta), xlim=c(0.04, 0.2))
+hist(Samples.simulate$beta, freq = FALSE, main = expression(paste('Histogram of ', beta, ' with simulation-based MCMC algorithm')),
+     xlab = expression(beta), xlim=c(0.04, 0.2))
 
-par(mfrow=c(2, 1))
+par(mfrow=c(3, 1))
 hist(Samples.new$gamma, freq = FALSE, main = expression(paste('Histogram of ', gamma, ' with new MCMC algorithm')),
      xlab = expression(gamma), xlim=c(0.03, 0.16))
 hist(Samples.current$gamma, freq = FALSE, main = expression(paste('Histogram of ', gamma, ' with current MCMC algorithm')),
      xlab = expression(gamma), xlim=c(0.03, 0.16))
+hist(Samples.simulate$gamma, freq = FALSE, main = expression(paste('Histogram of ', gamma, ' with simulation-based MCMC algorithm')),
+     xlab = expression(gamma), xlim=c(0.03, 0.16))
 
 mean(Samples.new$beta) #0.110
-mean(Samples.new$gamma) #0.084
-var(Samples.new$beta) #0.00032
-var(Samples.new$gamma) #0.00028
-ess(Samples.new$beta) #1588.848
-ess(Samples.new$gamma) #621.0135
+mean(Samples.new$gamma) #0.086
+var(Samples.new$beta) #0.00035
+var(Samples.new$gamma) #0.00030
+ess(Samples.new$beta) #1566.731
+ess(Samples.new$gamma) #784.1569
 
-mean(Samples.current$beta) #0.101
-mean(Samples.current$gamma) #0.077
-var(Samples.current$beta) #0.00045
-var(Samples.current$gamma) #0.00045
-ess(Samples.current$beta) #346.0594
-ess(Samples.current$gamma) #174.275
+mean(Samples.current$beta) #0.105
+mean(Samples.current$gamma) #0.081
+var(Samples.current$beta) #0.00046
+var(Samples.current$gamma) #0.00042
+ess(Samples.current$beta) #588.9186
+ess(Samples.current$gamma) #390.2475
+
+mean(Samples.simulate$beta) #
+mean(Samples.simulate$gamma) #
+var(Samples.simulate$beta) #
+var(Samples.simulate$gamma) #
+ess(Samples.simulate$beta) #
+ess(Samples.simulate$gamma) #
