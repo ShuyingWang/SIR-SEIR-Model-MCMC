25bd21622993d014338b8c37ec7975881d6d28dc
ShuyingWang <40690641+ShuyingWang@users.noreply.github.com>
2023-04-25 15:17:50 -0700

    Add files via upload

--- simulation1.R
+++ simulation1.R
@@ -145,6 +145,67 @@ Indicator <- function(t.x, t.y){
   return(ind)
 }
 
+# Conversions
+X_to_u <- function(t.x, t.y, t0, T.end, beta){
+  t.x = c(t.x, T.end)
+  t = sort(c(t.x, t.y, t0))
+  S = S0
+  I = I0
+  U = c()
+  u = 0
+  
+  for (i in 1:(length(t) - 1)){
+    u = u + (t[i+1] - t[i]) * S * I
+    if (t[i+1] %in% t.x){
+      S = S - 1
+      I = I + 1
+      U = c(U, u)
+      u = 0
+    }else{
+      I = I - 1
+    }
+  }
+  U = U * beta / N
+  n = length(U)
+  U[n] = U[n] + rexp(1, 1)
+  return(U)
+}
+u_to_u.new <- function(u, r){
+  n = length(u)
+  u.new = ifelse(rbinom(n, 1, r), rexp(n, 1), u)
+  return(u.new)
+}
+u_to_X <- function(u, t.y, t0, T.end, beta){
+  t.y = c(t.y, T.end)
+  n = length(u)
+  S = S0
+  I = I0
+  t.x = c()
+  t = t0
+  j = 1
+  i = 1
+  
+  while (t < T.end & S > 0 & I > 0){
+    
+    if ((t + u[i] / (beta*S*I/N)) < t.y[j]){
+      t = t + u[i] / (beta*S*I/N)
+      t.x = c(t.x, t)
+      S = S - 1
+      I = I + 1
+      i = i + 1
+      if (i > n){
+        u[i] = rexp(1, 1)
+      }
+    }else{
+      u[i] = u[i] - (t.y[j] - t) * (beta*S*I/N)
+      t = t.y[j]
+      I = I - 1
+      j = j + 1
+    }
+  }
+  return(t.x)
+}
+
 # New MCMC algorithm 
 New_proposal.x <- function(t.y, n.y, beta, t0, T.end){
   ty = c(t.y, T.end)
@@ -211,62 +272,60 @@ New_MH.x <- function(t.x, n.x, t.y, n.y, beta, gamma, t0, T.end, log_p){
   }
   return(list("tx"=t.x, "nx"=n.x, "lp"=log_p, "acc"=acc))
 }
+New_MH_adjust.x <- function(r, t.x, n.x, t.y, n.y, beta, gamma, t0, T.end, log_p){
+  u = X_to_u(t.x, t.y, t0, T.end, beta)
+  u.new = u_to_u.new(u, r)
+  tx.new = u_to_X(u.new, t.y, t0, T.end, beta)
+  nx.new = length(tx.new)
+  acc = 0
+  if (Indicator(tx.new, t.y)){
+    log_p.new = Log.I_prod(tx.new, t.y) - gamma * I_int(tx.new, t.y, nx.new, n.y, t0, T.end)
+    log_a = log_p.new-log_p
+    acc = 0
+    if (log(runif(1)) < log_a){
+      t.x = tx.new
+      n.x = nx.new
+      log_p = log_p.new
+      acc = 1
+    }
+  }
+  return(list("tx"=t.x, "nx"=n.x, "lp"=log_p, "acc"=acc))
+}
 
 # Current MCMC algorithm (Random Walk Metropolis)
-Current_MH.x <- function(t.x, n.x, t.y, n.y, beta, gamma, t0, T.end, log_p){
-  valid_proposal = FALSE
-  while (valid_proposal == FALSE){
+Current_MH.x <- function(k, t.x, n.x, t.y, n.y, beta, gamma, t0, T.end, log_p){
+  tx.new = t.x
+  log_a = 0
+  for (i in 1:k){
     u = runif(1, 0, 1)
-    if (n.x+I0 > n.y){
-      if (u < 1/3){
-        # Add one time point
-        t.new = runif(1, 0, T.end)
-        tx.new = sort(c(t.new, t.x))
-      }else{
-        if (u > 2/3){
-          # Move one time point
-          t.new = runif(1, 0, T.end)
-          tx.new = t.x[-sample(1:n.x, 1)]
-          tx.new = sort(c(t.new, tx.new))
-        }else{
-          # Remove one time point
-          tx.new = t.x[-sample(1:n.x, 1)]
-        }
-      }
-    }
-    else{
-      if (u < 1/2){
-        # Add one time point
-        t.new = runif(1, 0, T.end)
-        tx.new = sort(c(t.new, t.x))
-      }else{
+    if (u < 1/3){
+      # Add one time point
+      tx.new = sort(c(runif(1, t0, T.end), tx.new))
+      log_a = log_a + log(T.end-t0) - log(length(tx.new))
+    }else{
+      if (u > 2/3){
         # Move one time point
-        t.new = runif(1, 0, T.end)
-        tx.new = t.x[-sample(1:n.x, 1)]
-        tx.new = sort(c(t.new, tx.new))
+        tx.new = tx.new[-sample(1:length(tx.new), 1)]
+        tx.new = sort(c(runif(1, t0, T.end), tx.new))
+      }else{
+        # Remove one time point
+        tx.new = tx.new[-sample(1:length(tx.new), 1)]
+        log_a = log_a + log(length(tx.new)+1) - log(T.end-t0)
       }
     }
-    valid_proposal = Indicator(tx.new, t.y)
   }
   nx.new = length(tx.new)
-  log_p.new = nx.new * log(beta/N) + Log.SI_prod(tx.new, t.y) - beta/N * SI_int(tx.new, t.y, nx.new, n.y, t0, T.end) +
-    Log.I_prod(tx.new, t.y) - gamma * I_int(tx.new, t.y, nx.new, n.y, t0, T.end)
-  if (nx.new-n.x == 1){
-    # Add one time point
-    log_a = log_p.new + log(T.end-t0) - (log_p + log(n.x+1))
-  }else if(nx.new-n.x == -1){
-    # Remove one time point
-    log_a = log_p.new + log(n.x) - (log_p + log(T.end-t0))
-  }else{
-    # Move one time point
-    log_a = log_p.new - log_p
-  }
   acc = 0
-  if (log(runif(1)) < log_a){
-    t.x = tx.new
-    n.x = nx.new
-    log_p = log_p.new
-    acc = 1
+  if (Indicator(tx.new, t.y)){
+    log_p.new = nx.new * log(beta/N) + Log.SI_prod(tx.new, t.y) - beta/N * SI_int(tx.new, t.y, nx.new, n.y, t0, T.end) +
+      Log.I_prod(tx.new, t.y) - gamma * I_int(tx.new, t.y, nx.new, n.y, t0, T.end)
+    log_a = log_a + log_p.new - log_p
+    if (log(runif(1)) < log_a){
+      t.x = tx.new
+      n.x = nx.new
+      log_p = log_p.new
+      acc = 1
+    }
   }
   return(list("tx"=t.x, "nx"=n.x, "lp"=log_p, "acc"=acc))
 }
@@ -411,122 +470,8 @@ Distance_matrix <- function(x, y){
 
 ### 2. Illustrations with simulated data ###
 
-## Small epidemic size
-N = 100
-I0 = 3
-S0 = N - I0
-beta = 1.5
-gamma = 1
-t0 = 0
-T.end = 3
-
-# Simulate data
-set.seed(2)
-t = Sim_SIR(N, I0, beta, gamma, T.end)
-t.x = t$x
-t.y = t$y
-nx.true = length(t.x)
-n.y = length(t.y)
-
-# Compare proposal distributions
-B = 10000
-Sample_proposal.new = rep(NA, B)
-Sample_proposal.simulation = rep(NA, B)
-for (i in 1:B){
-  Sample_proposal.new[i] = length(New_proposal.x(t.y, n.y, beta, 0, T.end))
-  Sample_proposal.simulation[i] = length(Simulation_proposal.x(t.y, n.y, beta, 0, T.end))
-}
-
-# Compare MCMC samples
-B = 10000
-n.x = n.y
-t.x = sort(t.y - rexp(n.y, gamma))
-t.x = ifelse(t.x<0, 0, t.x)
-log_p = Log.I_prod(t.x, t.y) - gamma * I_int(t.x, t.y, n.x, n.y, t0, T.end)
-sample.new = rep(NA, B)
-acc.new = 0
-for (i in 1:B){
-  Sample = New_MH.x(t.x, n.x, t.y, n.y, beta, gamma, t0, T.end, log_p)
-  t.x = Sample$tx
-  n.x = Sample$nx
-  log_p = Sample$lp
-  acc.new = acc.new + Sample$acc
-  sample.new[i] = n.x
-}
-
-n.x = n.y
-t.x = sort(t.y - rexp(n.y, gamma))
-t.x = ifelse(t.x<0, 0, t.x)
-log_p = Log.I_prod(t.x, t.y) - gamma * I_int(t.x, t.y, n.x, n.y, t0, T.end)
-sample.simulation = rep(NA, B)
-acc.simulation = 0
-for (i in 1:B){
-  Sample = Simulation_MH.x(t.x, n.x, t.y, n.y, beta, gamma, t0, T.end, log_p)
-  t.x = Sample$tx
-  n.x = Sample$nx
-  log_p = Sample$lp
-  acc.simulation = acc.simulation + Sample$acc
-  sample.simulation[i] = n.x
-}
-
-n.x = n.y
-t.x = sort(t.y - rexp(n.y, gamma))
-t.x = ifelse(t.x<0, 0, t.x)
-log_p = n.x * log(beta/N) + Log.SI_prod(t.x, t.y) - beta/N * SI_int(t.x, t.y, n.x, n.y, t0, T.end) +
-  Log.I_prod(t.x, t.y) - gamma * I_int(t.x, t.y, n.x, n.y, t0, T.end)
-sample.current = rep(NA, B)
-acc.current = 0
-for (i in 1:B){
-  Sample = Current_MH.x(t.x, n.x, t.y, n.y, beta, gamma, t0, T.end, log_p)
-  t.x = Sample$tx
-  n.x = Sample$nx
-  log_p = Sample$lp
-  acc.current = acc.current + Sample$acc
-  sample.current[i] = n.x
-}
-
-# Acceptance rates
-acc.new/B #0.1641
-acc.simulation/B #0.0622
-acc.current/B #0.7763
-
-# Effective sample sizes
-ess(sample.new) #699.89
-ess(sample.simulation) #426.20
-ess(sample.current) #47.54
-
-Samples.small = list(Sample_proposal.new, Sample_proposal.simulation, sample.new, sample.simulation, sample.current)
-save(Samples.small, file='small.RData')
-
-# Plot
-par(mfrow = c(3, 1))
-hist(Sample_proposal.new, xlab=expression(n[x]), freq=FALSE, breaks=20, main='New proposal', ylim=c(0, 0.14))
-hist(Sample_proposal.simulation, xlab=expression(n[x]), freq=FALSE, breaks=20, main='Simulation-based proposal', ylim=c(0, 0.14))
-hist(sample.new, breaks=20, xlab=expression(n[x]), freq=FALSE, xlim=c(20, 95), ylim=c(0, 0.14), main='Target distribution')
-
-par(mfrow = c(3, 1))
-plot(sample.new, type='l', ylab=expression(n[x]), main='New MCMC algorithm')
-plot(sample.simulation, type='l', ylab=expression(n[x]), main='Simulation-based MCMC algorithm')
-plot(sample.current, type='l', ylab=expression(n[x]), main='Current MCMC algorithm')
-
-par(mfrow = c(3, 1))
-acf(sample.new,  main='New MCMC algorithm')
-acf(sample.simulation,  main='Simulation-based MCMC algorithm')
-acf(sample.current, main='Current MCMC algorithm')
-
-# Sample distance between proposal and target by cross match
-label = c(rep(0, 2000), rep(1, 2000))
-sample.target = sample.new[5*(1:2000)]
-crossmatch.new = crossmatchtest(label, Distance_matrix(Sample_proposal.new[1:2000], sample.target))
-crossmatch.new$a1 / crossmatch.new$Ea1 #0.812
-crossmatch.simulation = crossmatchtest(label, Distance_matrix(Sample_proposal.simulation[1:2000], sample.target))
-crossmatch.simulation$a1 / crossmatch.simulation$Ea1 #0.370
-
-
-
-
-
-## Moderate epidemic size
+## Setting
+# Small
 N = 1000
 I0 = 10
 S0 = N - I0
@@ -535,24 +480,62 @@ gamma = 1
 t0 = 0
 T.end = 3
 
-# Simulate data
+# Moderate
+N = 1000
+I0 = 20
+S0 = N - I0
+beta = 2
+gamma = 1
+t0 = 0
+T.end = 3
+
+# Large
+N = 1000
+I0 = 50
+S0 = N - I0
+beta = 2.5
+gamma = 1
+t0 = 0
+T.end = 5
+
+
+
+
+
+## Simulate data
 set.seed(3)
 t = Sim_SIR(N, I0, beta, gamma, T.end)
 t.x = t$x
 t.y = t$y
 nx.true = length(t.x)
 n.y = length(t.y)
+nx.true
+n.y
 
-# Compare proposal distributions
+
+
+
+
+## Compare proposal distributions
 B = 10000
 Sample_proposal.new = rep(NA, B)
 Sample_proposal.simulation = rep(NA, B)
 for (i in 1:B){
   Sample_proposal.new[i] = length(New_proposal.x(t.y, n.y, beta, 0, T.end))
   Sample_proposal.simulation[i] = length(Simulation_proposal.x(t.y, n.y, beta, 0, T.end))
+  print(i)
 }
+par(mfrow = c(2, 1))
+hist(Sample_proposal.new, xlab=expression(n[x]), freq=FALSE, breaks=20, main='New proposal')
+hist(Sample_proposal.simulation, xlab=expression(n[x]), freq=FALSE, breaks=20, main='Simulation-based proposal')
 
-# Compare MCMC samples
+
+
+
+
+## Compare MCMC samples
+# New MCMC
+set.seed(3)
 B = 10000
 n.x = n.y
 t.x = sort(t.y - rexp(n.y, gamma))
@@ -560,15 +543,26 @@ t.x = ifelse(t.x<0, 0, t.x)
 log_p = Log.I_prod(t.x, t.y) - gamma * I_int(t.x, t.y, n.x, n.y, t0, T.end)
 sample.new = rep(NA, B)
 acc.new = 0
+# r = 0.1
+# r = 0.03
 for (i in 1:B){
-  Sample = New_MH.x(t.x, n.x, t.y, n.y, beta, gamma, t0, T.end, log_p)
+  # Sample = New_MH.x(t.x, n.x, t.y, n.y, beta, gamma, t0, T.end, log_p)
+  Sample = New_MH_adjust.x(r, t.x, n.x, t.y, n.y, beta, gamma, t0, T.end, log_p)
   t.x = Sample$tx
   n.x = Sample$nx
   log_p = Sample$lp
   acc.new = acc.new + Sample$acc
   sample.new[i] = n.x
+  print(i)
 }
+acc.new/B # 0.1197, 0.1613, 0.3032
+ess(sample.new) # 885.83, 549.16, 467.84
+mean(sample.new) # 115.5, 418.9, 824.1
+plot(sample.new, type='l', ylab=expression(n[x]), main='New MCMC algorithm')
 
+
+# Simulation based MCMC
+set.seed(3)
 n.x = n.y
 t.x = sort(t.y - rexp(n.y, gamma))
 t.x = ifelse(t.x<0, 0, t.x)
@@ -582,8 +576,16 @@ for (i in 1:B){
   log_p = Sample$lp
   acc.simulation = acc.simulation + Sample$acc
   sample.simulation[i] = n.x
+  print(i)
 }
+acc.simulation/B # 0.0447, 0.0425, 0.0377
+ess(sample.simulation) # 362.06, 199.18, 190.41
+mean(sample.simulation) # 115.0, 419.5, 824.1
+plot(sample.simulation, type='l', ylab=expression(n[x]), main='Simulation-based MCMC algorithm')
 
+
+# Random walk MCMC
+set.seed(3)
 n.x = n.y
 t.x = sort(t.y - rexp(n.y, gamma))
 t.x = ifelse(t.x<0, 0, t.x)
@@ -591,148 +593,55 @@ log_p = n.x * log(beta/N) + Log.SI_prod(t.x, t.y) - beta/N * SI_int(t.x, t.y, n.
   Log.I_prod(t.x, t.y) - gamma * I_int(t.x, t.y, n.x, n.y, t0, T.end)
 sample.current = rep(NA, B)
 acc.current = 0
+k = 20
+# k = 20
+# k = 5
 for (i in 1:B){
-  Sample = Current_MH.x(t.x, n.x, t.y, n.y, beta, gamma, t0, T.end, log_p)
+  Sample = Current_MH.x(k, t.x, n.x, t.y, n.y, beta, gamma, t0, T.end, log_p)
   t.x = Sample$tx
   n.x = Sample$nx
   log_p = Sample$lp
   acc.current = acc.current + Sample$acc
   sample.current[i] = n.x
+  print(i)
 }
+acc.current/B # 0.3114, 0.2289, 0.3137
+ess(sample.current) # 63.95, 18.12, 22.58
+mean(sample.current[1000:10000]) # 114.8, 411.1, 824.3
+plot(sample.current, type='l', ylab=expression(n[x]), main='Current MCMC algorithm')
 
-# Acceptance rates
-acc.new/B #0.1197
-acc.simulation/B #0.0447
-acc.current/B #0.8088
 
-# Effective sample sizes
-ess(sample.new) #885.83
-ess(sample.simulation) #362.06
-ess(sample.current) #7.48
+
+
+
+## Results
+Samples.small = list(Sample_proposal.new, Sample_proposal.simulation, sample.new, sample.simulation, sample.current)
+save(Samples.small, file='small.RData')
 
 Samples.moderate = list(Sample_proposal.new, Sample_proposal.simulation, sample.new, sample.simulation, sample.current)
 save(Samples.moderate, file='moderate.RData')
 
-# Plot
-par(mfrow = c(3, 1))
-hist(Sample_proposal.new, xlab=expression(n[x]), freq=FALSE, breaks=20, ylim=c(0, 0.035), main='New proposal')
-hist(Sample_proposal.simulation, xlab=expression(n[x]), freq=FALSE, breaks=20, ylim=c(0, 0.035), main='Simulation-based proposal')
-hist(sample.new, breaks=20, xlab=expression(n[x]), freq=FALSE, xlim=c(60, 600), ylim=c(0, 0.035), main='Target distribution')
-
-par(mfrow = c(3, 1))
-plot(sample.new, type='l', ylab=expression(n[x]), main='New MCMC algorithm')
-plot(sample.simulation, type='l', ylab=expression(n[x]), main='Simulation-based MCMC algorithm')
-plot(sample.current, type='l', ylab=expression(n[x]), main='Current MCMC algorithm')
-
-par(mfrow = c(3, 1))
-acf(sample.new,  main='New MCMC algorithm')
-acf(sample.simulation,  main='Simulation-based MCMC algorithm')
-acf(sample.current, main='Current MCMC algorithm')
-
-# Sample distance between proposal and target by cross match
-label = c(rep(0, 2000), rep(1, 2000))
-sample.target = sample.new[5*(1:2000)]
-crossmatch.new = crossmatchtest(label, Distance_matrix(Sample_proposal.new[1:2000], sample.target))
-crossmatch.new$a1 / crossmatch.new$Ea1 #0.482
-crossmatch.simulation = crossmatchtest(label, Distance_matrix(Sample_proposal.simulation[1:2000], sample.target))
-crossmatch.simulation$a1 / crossmatch.simulation$Ea1 #0.206
-
-
-
-
-
-## Large epidemic size
-N = 10000
-I0 = 50
-S0 = N - I0
-beta = 1.5
-gamma = 1
-t0 = 0
-T.end = 3
-
-# Simulate data
-set.seed(1)
-t = Sim_SIR(N, I0, beta, gamma, T.end)
-t.x = t$x
-t.y = t$y
-nx.true = length(t.x)
-n.y = length(t.y)
-
-# Compare proposal distributions
-B = 10000
-Sample_proposal.new = rep(NA, B)
-Sample_proposal.simulation = rep(NA, B)
-for (i in 1:B){
-  Sample_proposal.new[i] = length(New_proposal.x(t.y, n.y, beta, 0, T.end))
-  Sample_proposal.simulation[i] = length(Simulation_proposal.x(t.y, n.y, beta, 0, T.end))
-}
-
-# MCMC samples
-B = 10000
-n.x = n.y
-t.x = sort(t.y - rexp(n.y, gamma))
-t.x = ifelse(t.x<0, 0, t.x)
-log_p = Log.I_prod(t.x, t.y) - gamma * I_int(t.x, t.y, n.x, n.y, t0, T.end)
-sample.new = rep(NA, B)
-acc.new = 0
-for (i in 1:B){
-  Sample = New_MH.x(t.x, n.x, t.y, n.y, beta, gamma, t0, T.end, log_p)
-  t.x = Sample$tx
-  n.x = Sample$nx
-  log_p = Sample$lp
-  acc.new = acc.new + Sample$acc
-  sample.new[i] = n.x
-}
-
-n.x = n.y
-t.x = sort(t.y - rexp(n.y, gamma))
-t.x = ifelse(t.x<0, 0, t.x)
-log_p = Log.I_prod(t.x, t.y) - gamma * I_int(t.x, t.y, n.x, n.y, t0, T.end)
-sample.simulation = rep(NA, B)
-acc.simulation = 0
-for (i in 1:B){
-  Sample = Simulation_MH.x(t.x, n.x, t.y, n.y, beta, gamma, t0, T.end, log_p)
-  t.x = Sample$tx
-  n.x = Sample$nx
-  log_p = Sample$lp
-  acc.simulation = acc.simulation + Sample$acc
-  sample.simulation[i] = n.x
-}
-
-n.x = n.y
-t.x = sort(t.y - rexp(n.y, gamma))
-t.x = ifelse(t.x<0, 0, t.x)
-log_p = n.x * log(beta/N) + Log.SI_prod(t.x, t.y) - beta/N * SI_int(t.x, t.y, n.x, n.y, t0, T.end) +
-  Log.I_prod(t.x, t.y) - gamma * I_int(t.x, t.y, n.x, n.y, t0, T.end)
-sample.current = rep(NA, B)
-acc.current = 0
-for (i in 1:B){
-  Sample = Current_MH.x(t.x, n.x, t.y, n.y, beta, gamma, t0, T.end, log_p)
-  t.x = Sample$tx
-  n.x = Sample$nx
-  log_p = Sample$lp
-  acc.current = acc.current + Sample$acc
-  sample.current[i] = n.x
-}
-
-# Acceptance rates
-acc.new/B #0.0527
-acc.simulation/B #0.0439
-acc.current/B #0.8275
-
-# Effective sample sizes
-ess(sample.new) #359.98
-ess(sample.simulation) #341.14 
-ess(sample.current) #5.81
-
 Samples.large = list(Sample_proposal.new, Sample_proposal.simulation, sample.new, sample.simulation, sample.current)
 save(Samples.large, file='large.RData')
 
-# Plot
+
+# Acceptance rates
+acc.new/B # 0.1197, 0.1613, 0.3032
+acc.simulation/B # 0.0447, 0.0425, 0.0377
+acc.current/B # 0.3114, 0.2289, 0.3137
+
+
+# Effective sample sizes
+ess(sample.new) # 885.83, 549.16, 467.84
+ess(sample.simulation) # 362.06, 199.18, 190.41
+ess(sample.current) # 63.95, 18.12, 22.58
+
+
+# Plots
 par(mfrow = c(3, 1))
-hist(Sample_proposal.new, xlab=expression(n[x]), freq=FALSE, breaks=20, xlim=c(200, 2000), ylim=c(0, 0.005), main='New proposal')
-hist(Sample_proposal.simulation, xlab=expression(n[x]), freq=FALSE, breaks=20, xlim=c(200, 2000), ylim=c(0, 0.005), main='Simulation-based proposal')
-hist(sample.new, breaks=20, xlab=expression(n[x]), freq=FALSE, xlim=c(200, 2000), ylim=c(0, 0.02), main='Target distribution')
+hist(Sample_proposal.new, xlab=expression(n[x]), freq=FALSE, breaks=20, main='New proposal', ylim=c(0, 0.05), xlim=c(765, 940))
+hist(Sample_proposal.simulation, xlab=expression(n[x]), freq=FALSE, breaks=20, main='Simulation-based proposal', ylim=c(0, 0.05), xlim=c(765, 940))
+hist(sample.new, breaks=20, xlab=expression(n[x]), freq=FALSE, ylim=c(0, 0.05), main='Target distribution', xlim=c(765, 940))
 
 par(mfrow = c(3, 1))
 plot(sample.new, type='l', ylab=expression(n[x]), main='New MCMC algorithm')
@@ -744,10 +653,16 @@ acf(sample.new,  main='New MCMC algorithm')
 acf(sample.simulation,  main='Simulation-based MCMC algorithm')
 acf(sample.current, main='Current MCMC algorithm')
 
+
 # Sample distance between proposal and target by cross match
 label = c(rep(0, 2000), rep(1, 2000))
 sample.target = sample.new[5*(1:2000)]
 crossmatch.new = crossmatchtest(label, Distance_matrix(Sample_proposal.new[1:2000], sample.target))
-crossmatch.new$a1 / crossmatch.new$Ea1 #0.184
+1 - crossmatch.new$a1 / crossmatch.new$Ea1 #0.518, 0.592, 0.536
 crossmatch.simulation = crossmatchtest(label, Distance_matrix(Sample_proposal.simulation[1:2000], sample.target))
-crossmatch.simulation$a1 / crossmatch.simulation$Ea1 #0.158
+1 - crossmatch.simulation$a1 / crossmatch.simulation$Ea1 #0.794, 0.788, 0.736
+
+
+
+
+
